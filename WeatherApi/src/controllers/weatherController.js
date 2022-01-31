f4c34a23ee4ec356190c2abb6e0a053d4a32df3c
soorajks21 <soorajks@live.in>
2022-01-30 23:52:44 -0500

    Feat: Error handing
    Add error handler on the controller to catch exception

--- WeatherApi/src/controllers/weatherController.js
+++ WeatherApi/src/controllers/weatherController.js
@@ -1,11 +1,17 @@
 const { getWeatherData, fetchWeatherApi } = require('../Utility/HelperFunctions');
 
-const getWeatherDetails = async (req, res) => {
-    
-    const weatherDetails = await fetchWeatherApi();
 
-    const data = getWeatherData(weatherDetails);
-    return res.status(200).send({weather: data});
+const getWeatherDetails = async (req, res) => {
+    try {
+        const weatherDetails = await fetchWeatherApi();
+
+        const data = getWeatherData(weatherDetails);
+        return res.status(200).send({weather: data});
+    }
+    catch (ex) {
+        return res.status(400).send({ error:'Internal Server Error'});
+    }
+ 
     };
 
 
