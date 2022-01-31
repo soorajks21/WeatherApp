5ec2105037603cca3a01f95c67cb118a1243b90b
soorajks21 <soorajks@live.in>
2022-01-31 00:53:55 -0500

    Ref: refactor code

--- weather-spa/src/App.tsx
+++ weather-spa/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Weather from './components/WeatherComponent/Weather';
-import cloudy from './components/WeatherComponent/icons/perfectday.svg';
+import Logo from './components/WeatherComponent/icons/perfectday.svg';
 import styled from 'styled-components';
 
 const Heading = styled.h1`
@@ -14,7 +14,7 @@ function App() {
      <nav className="navbar">
         <div className="navbar-brand">
             <a className="navbar-item" href="https://github.com/soorajks21/WeatherApp">
-            <img src={cloudy} alt="WEATHER APP" width="50px" height="50px" />
+            <img src={Logo} alt="WEATHER APP" width="50px" height="50px" />
             <Heading>Weather App</Heading>
             </a>
         </div>
