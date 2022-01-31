d7eba2a8b02dda30136d5f67369773b60870a5ea
soorajks21 <soorajks@live.in>
2022-01-31 08:59:51 -0500

    Config: API Version Add version to the api and access point in the config file

--- WeatherApi/src/routers/weather.js
+++ WeatherApi/src/routers/weather.js
@@ -2,12 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { getWeatherDetails } = require('../controllers/weatherController');
 
+const api_point = process.env.API_POINT;
 
-const api = process.env.API_URL;
-
-console.log(api);
-
-
-router.get('/', getWeatherDetails);
+router.get(`${api_point}`+'/', getWeatherDetails);
 
 module.exports = router;
