c0a73ff34780be80fe0cfef8b716829c7f6690d8
soorajks21 <soorajks@live.in>
2022-01-31 01:00:09 -0500

    Refactor: refactoring the code

--- weather-spa/src/components/WeatherComponent/WeatherInfoComponent.tsx
+++ weather-spa/src/components/WeatherComponent/WeatherInfoComponent.tsx
@@ -1,5 +1,4 @@
 import React, { FC } from 'react';
-import styled from "styled-components"
 import { WeatherInfoIcons, InfoContainer, InfoIcon, InfoLabel } from './WeatherStyledComponent';
 
 
@@ -9,37 +8,40 @@ import { WeatherInfoIcons, InfoContainer, InfoIcon, InfoLabel } from './WeatherS
     return (
 
         <>
-        <InfoContainer>
-
-            <InfoIcon src={WeatherInfoIcons.sunset}></InfoIcon>
-                <InfoLabel>max
-                {max &&
-                         <span>
-                             {max} C
-                         </span>
-                    }
-                </InfoLabel>
-    
-            </InfoContainer>
-          <InfoContainer>
+             <InfoContainer>
                 <InfoIcon src={ WeatherInfoIcons.pressure}></InfoIcon>
                 <InfoLabel>
                     time
                     <span>{time}</span>
             </InfoLabel>
             </InfoContainer>
-         <InfoContainer>
-            <InfoIcon src={ WeatherInfoIcons.sunset}></InfoIcon>
+             <InfoContainer>
+                <InfoIcon src={WeatherInfoIcons.sunset}></InfoIcon>
                 <InfoLabel>min
                     {min &&
-                         <span>
-                             {min} C
-                         </span>
+                        <span>
+                            {min} C
+                        </span>
                     }
                    
                 </InfoLabel>
                 
-                </InfoContainer>
+            </InfoContainer>
+         
+            <InfoContainer>
+
+                <InfoIcon src={WeatherInfoIcons.sunset}></InfoIcon>
+                <InfoLabel>max
+                    {max &&
+                        <span>
+                            {max} C
+                        </span>
+                    }
+                </InfoLabel>
+    
+            </InfoContainer>
+         
+       
         </>
     
     )
